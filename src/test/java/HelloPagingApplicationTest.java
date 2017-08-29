1ac2549a300869e24a35557354cd2a8bee9e456c
Олег <_oleg@ua.fm>
2017-08-29 22:39:36 +0300

    First test added

--- src/test/java/HelloPagingApplicationTest.java
+++ src/test/java/HelloPagingApplicationTest.java
@@ -1,12 +1,23 @@
+import hello.HelloPagingApplication;
+import hello.controller.WebController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = HelloPagingApplication.class)
 public class HelloPagingApplicationTest {
-//@Test
-//    public void contextLoads() {
-//    }
-}
+    @Autowired
+    private WebController controller;
+
+    @Test
+        public void contexLoads(){
+            assertThat(controller).isNotNull();
+        }
+    }
+
+
