735dcdc42984adeaa8a83171557ece1f6422329d
Олег <_oleg@ua.fm>
2017-08-31 22:16:59 +0300

    Some tests added

--- src/test/java/HelloPagingApplicationTest.java
+++ src/test/java/HelloPagingApplicationTest.java
@@ -2,13 +2,19 @@ import hello.HelloPagingApplication;
 import hello.controller.WebController;
 import hello.model.Contact;
 import hello.service.ContactService;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HelloPagingApplication.class)
@@ -17,6 +23,20 @@ public class HelloPagingApplicationTest {
     private WebController controller;
     @Autowired
     private ContactService contactService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+
+        contactService.deleteAll();
+        this.mockMvc = standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void findContactMustReturnJSON() throws Exception {
+
+    }
 
     @Test
     public void contexLoads() {
@@ -35,6 +55,14 @@ public class HelloPagingApplicationTest {
         contactService.save(new Contact("^.*[aei].*$"));
         assertThat(controller.findAll("^.*[aei].*$")).isEqualToIgnoringCase("contacts: []");
     }
+
+    @Test
+    public void testContactClass(){
+        Contact contact = new Contact("test");
+        assertThat(contact.getName()).matches("test");
+    }
+
+
 }
 
 
